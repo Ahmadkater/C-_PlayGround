b7ff5cbafc8079555a0cf39a5b9c8581712976aa
AhmadKater <ahmed.said3439@yahoo.com>
2021-07-08 22:02:24 +0200

    Using Object To Support Any Type

--- C# Generics/WiredBrainCoffee/StackApp/Program.cs
+++ C# Generics/WiredBrainCoffee/StackApp/Program.cs
@@ -7,6 +7,7 @@ namespace WiredBrainCoffee.StackApp
         static void Main(string[] args)
         {
             doubleStack();
+            stringStack();
         }
 
         public static void doubleStack()
@@ -15,6 +16,21 @@ namespace WiredBrainCoffee.StackApp
             s.Push(25.0);
             s.Push(75.0);
             s.Push(100.0);
+
+            while (s.count > 0)
+            {
+                var item = s.Pop();
+
+                System.Console.WriteLine(item);
+            }
+        }
+
+        public static void stringStack()
+        {
+            var s = new SimpleStack();
+            s.Push("p1");
+            s.Push("p2");
+            s.Push("p3");
 
             while (s.count > 0)
             {
